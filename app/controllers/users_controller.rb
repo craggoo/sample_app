7af616599023fc56c3bf1a4bec9d0639f0e61326
Adam Cragg <craggoo@gmail.com>
2014-06-09 16:22:17 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
-  def new
+  def show
+  	@User = User.find(params[:id])
   end
+  def new
+  	@User = User.new
+  end
+  def create
+  	@User = User.new(user_params)
+  	if @User.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @User
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
 end
+
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name		"Adam Cragg"
+		email		"Craggoo@gmail.com"
+		password	"starcraft"
+		password_confirmation	"starcraft"
+	end
+end
