1159753ca55a9f16d14394b8f20510d5fe510eec
Adam Cragg <craggoo@gmail.com>
2014-06-12 17:22:57 -0700

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+
+  def destroy
+    @User.destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+
+
+
   def show
   	@User = User.find(params[:id])
   end
+  
   def new
   	@User = User.new
   end
+
+  def edit
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
   def create
   	@User = User.new(user_params)
   	if @User.save
@@ -16,10 +37,36 @@ class UsersController < ApplicationController
   	end
   end
 
+  def update
+    if @User.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @User
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   	def user_params
   		params.require(:user).permit(:name, :email, :password, :password_confirmation)
   	end
-end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." unless signed_in?
+      end
+    end
+
+    def correct_user
+      @User = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@User)
+    end
+
+    def admin_user
+      @User = User.find(params[:id])
+      redirect_to(root_url) unless current_user.admin?
+    end
+  end
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name		"Adam Cragg"
-		email		"Craggoo@gmail.com"
+		sequence(:name) {|n| "Person #{n}" }
+		sequence(:email) {|n| "person_#{n}@example.com" }
 		password	"starcraft"
 		password_confirmation	"starcraft"
+	end
+
+	factory :admin do
+		admin true
 	end
 end
